d6f289f5be81c7febcf242ea857423ec00466e54
Gayatri Pawar <kittupawar0911@gmail.com>
2026-06-29 11:14:50 +0530

    Fix Task model import path

--- Backend/routes/taskRoutes.js
+++ Backend/routes/taskRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Task = require("../models/Task");
+const Task = require("../models/Task.js");
 
 // GET All Tasks
 router.get("/", async (req, res) => {
